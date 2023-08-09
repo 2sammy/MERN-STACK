6ca45602575103ca6a49880a5c5f2bff059339ee
2sammy <samuelmanoah61@gmail.com>
2023-08-09 21:59:56 +0300

    added some changes

--- mern stack/frontend/src/index.js
+++ mern stack/frontend/src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { WorkoutsContextProvider } from './context/WorkoutContext';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+
+<WorkoutsContextProvider>
+
     <App />
+
+    </WorkoutsContextProvider>
+
+
   </React.StrictMode>
 );
 
--- mern stack/frontend/src/pages/Home.js
+++ mern stack/frontend/src/pages/Home.js
@@ -1,12 +1,35 @@
-import { useEffect, useState } from "react"
+ import { useEffect, useState } from "react"
+ import { useWorkoutContext } from "../hooks/useWorkoutsContext"
+
+// components
+import WorkoutDetails from "../components/WorkoutDetails"
+import WorkoutForm from "../components/WorkoutForm"
 
 const Home = () => {
+  const [workouts, setWorkouts] = useState(null)
 
-    useEffect()
-    return (
-        <div className="home">
-            <h2>Home</h2>
-        </div>
-    )
+  useEffect(() => {
+    const fetchWorkouts = async () => {
+      const response = await fetch('/api/workouts')
+      const json = await response.json()
+
+      if (response.ok) {
+        setWorkouts(json)
+      }
+    }
+
+    fetchWorkouts()
+  }, [])
+
+  return (
+    <div className="home">
+      <div className="workouts">
+        {workouts && workouts.map(workout => (
+          <WorkoutDetails workout={workout} key={workout._id} />
+        ))}
+      </div>
+    </div>
+  )
 }
+
 export default Home
