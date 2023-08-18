66b28e7191ccdc4bcc4c5488ce1caf9cd7db4a03
2sammy <samuelmanoah61@gmail.com>
2023-08-18 07:31:54 +0300

    added a form

--- mern stack/backend/server.js
+++ mern stack/backend/server.js
@@ -2,6 +2,7 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors')
 const workoutRoutes = require('./routes/workouts');
 
 // express app
@@ -10,6 +11,7 @@ const app = express();
 
 // building a middleware
 app.use(express.json());
+app.use(cors());
 
 app.use((req, res, next) => {
     console.log(req.path, req.method)
