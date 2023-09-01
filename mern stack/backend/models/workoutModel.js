4a3941275b82c0741e21e0c08e38aa4b313c7e8b
2sammy <samuelmanoah61@gmail.com>
2023-09-01 07:27:14 +0300

    schema

--- mern stack/backend/models/workoutModel.js
+++ mern stack/backend/models/workoutModel.js
@@ -7,24 +7,21 @@ const Schema = mongoose.Schema
 
 const workoutSchema = new Schema({
     title: {
-        type: String,
-        require: true
+        type : String,
+        require : true
     },
-
     reps : {
-        type: Number,
-        require: true
+        type: String,
+        require : true
     },
+    load : {
+        type : String,
+        require : true
 
-    load: {
-        type: Number,
-        require: true
     }
-},
+    
+}, {
+    timeseries: true
+})
 
-    {
-        timestamps: true
-    }
-)
-
-module.exports = mongoose.model('workout',workoutSchema)
+module.exports = mongoose.model('Workout',workoutSchema)
--- mern stack/workout/routes/workouts.js
+++ mern stack/workout/routes/workouts.js
@@ -1,40 +1,34 @@
 const express = require('express')
-const router = express.Router();
-const Workout = require('../models/workoutModel')
+const route = require('../../backend/routes/workouts')
+
+const router = express.Router()
 
 //Get all workouts
-router.get('/' , (req, res) => {
-res.json({message: "Get all workouts"})
-})
+router.get('/', (req, res) => {
+    res.json({msg: 'Get all workouts'})
 
+})
 //get single workout
-router.get('/:id' , (req, res) => {
-res.json({message: "Get single workout"})
+router.get('/:id', (req, res) => {
+    res.json({msg: 'Get  single workouts'})
+
 })
 
-// post new workout
-router.post('/' , async (req, res) => {
-    const {title,load,reps} = req.body
-    try{
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
+//post new workout
+router.post('/', (req, res) => {
+    res.json({msg: 'Get  single workouts'})
 
-    }
-    catch(error) {
-        res.status(400).json({error: error.message})
-
-    }
 })
-
 //delete workout
-router.delete('/:id' , (req, res) => {
-res.json({message: "delete  workout"})
+router.delete('/:id', (req, res) => {
+    res.json({msg: 'delete workouts'})
+
 })
 
 //update workout
-router.patch('/:id' , (req, res) => {
-res.json({message: "update a  workout"})
-})
+router.patch('/:id', (req, res) => {
+    res.json({msg: 'update   workouts'})
 
+})
 
 module.exports = router
