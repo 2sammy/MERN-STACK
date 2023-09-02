13c3a9c9ef96cf5d0e5ad9bb275f4b2ece70333d
2sammy <samuelmanoah61@gmail.com>
2023-09-02 10:48:03 +0300

    added

--- mern stack/frontend/src/App.js
+++ mern stack/frontend/src/App.js
@@ -1,28 +1,32 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-// pages & components
-import Home  from './pages/Home';
+//pages and components
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
+  return(
+    <div className='App'>
       <BrowserRouter>
       <Navbar />
+
       <div className='pages'>
         <Routes>
-          <Route
-            path= "/"
-            element = {<Home />}
-          />
-        </Routes>
-      </div>
 
+        <Route
+          path="/"
+          element= {<Home />}
+        />
+        </Routes>
+
+      </div>
       
       </BrowserRouter>
-     
     </div>
-  );
+
+  )
 }
+
+
 
 export default App;
--- mern stack/frontend/src/components/Navbar.js
+++ mern stack/frontend/src/components/Navbar.js
@@ -1,16 +1,18 @@
 import { Link } from "react-router-dom"
 
 const Navbar = () => {
+
     return(
         <header>
             <div className="container">
-                <Link to="/">
+                <Link to='/'>
                     <h1>Workout Buddy</h1>
                 </Link>
 
             </div>
-
         </header>
     )
+
 }
+
 export default Navbar
