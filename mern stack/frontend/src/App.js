10e141e027fb8792e97e56f1e18fc87fc3ae59cf
2sammy <samuelmanoah61@gmail.com>
2023-09-06 07:36:08 +0300

    navbar component

--- mern stack/frontend/src/App.js
+++ mern stack/frontend/src/App.js
@@ -30,3 +30,25 @@ function App() {
 
 
 export default App;
+  /**
+   * import {BrowserRouter, Routes, Route} from 'react-router-dom'
+   * .import Home from './pages/Home'
+   * 
+   * function App(){
+   * return(
+   * <div className="App">
+   * <BrowserRouter> 
+   * <div className=" pages">
+   * <Routes>
+   * <Route path='/'
+   * element={<Home />}
+   * />
+   * </Routes>
+   * </div>
+   * 
+   * )
+   * }
+   * 
+   * 
+   * 
+   */
