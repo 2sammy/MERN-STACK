566b0d4a9e1b65312f080b1f33623b2eb18d480b
2sammy <samuelmanoah61@gmail.com>
2023-09-07 22:27:44 +0300

    sign up set up

--- mern stack/backend/server.js
+++ mern stack/backend/server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors')
 const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
 
 // express app
 
@@ -19,6 +20,7 @@ app.use((req, res, next) => {
 })
 // react to the request by creating routes
 app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 // connect to the db
 mongoose.connect(process.env.MONGO_URI)
