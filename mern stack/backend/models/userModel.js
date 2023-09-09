5570bb067860d7e40d03e63f267be0ff9c29a99c
2sammy <samuelmanoah61@gmail.com>
2023-09-09 19:54:25 +0300

    jsonwebtoken

--- mern stack/backend/models/userModel.js
+++ mern stack/backend/models/userModel.js
@@ -14,8 +14,6 @@ const userSchema = new Schema({
         required: true
     }
 })
-
-
 //static signup method
 userSchema.statics.signup = async function (email, password){
     const exists = await this.findOne({ email})
