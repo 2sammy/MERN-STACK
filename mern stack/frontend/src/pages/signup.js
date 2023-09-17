19a3802e4b401506ae4bc4ed5b42197f4738859e
2sammy <samuelmanoah61@gmail.com>
2023-09-17 10:36:25 +0300

    hooks and pages

--- mern stack/frontend/src/pages/signup.js
+++ mern stack/frontend/src/pages/signup.js
@@ -1,34 +1,39 @@
 import { useState } from "react"
+import { useSignup } from "../hooks/useSignup"
 
 const Signup = () => {
+// we gonna use ustate to track what the user type in the input fields
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const {signup, error, isLoading} = useSignup('')
+
     const handleSubmit = async (e) => {
         e.preventDefault()
 
-        console.log(email,password)
+        await signup(email, password)
+
     }
 
     return(
-        <form className="signup" onClick={handleSubmit}>
-            <h3>Signup</h3>
+        //creating a form for signup
+        <form className="signup" onSubmit={handleSubmit}>
+            <h3>Sign up</h3>
+
             <label>Email :</label>
             <input
                 type="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
-            
-            
-            />
+             />
             <label>Password :</label>
             <input
                 type="password"
                 onChange={(e) => setPassword(e.target.value)}
                 value={password}
-            
-            
             />
-        <button>Signup</button>
+        <button disabled={isLoading}>Sign up</button>
+        {error && <div className="error">{error}</div>}
+
 
         </form>
     )
